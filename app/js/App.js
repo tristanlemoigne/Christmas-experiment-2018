0c5a1eb7316e2f11ad54e5ffd328208cb4386880
Tristan Le Moigne <tristanlemoigne@gmail.com>
2018-12-07 11:57:07 +0100

    Adding glass material

--- app/js/App.js
+++ app/js/App.js
@@ -17,10 +17,10 @@ export default class App {
         this.camera = new THREE.PerspectiveCamera(
             70,
             window.innerWidth / window.innerHeight,
-            0.1,
+            1,
             1000
         )
-        this.camera.position.set(0, 0, 30)
+        this.camera.position.set(0, 0, 10)
 
         // Renderer
         this.renderer = new THREE.WebGLRenderer({
@@ -47,7 +47,8 @@ export default class App {
             this.loadTexture("/app/assets/textures/fire.png", "fireTexture"),
             this.loadTexture("/app/assets/textures/flake-1.png","flake1Texture"),
             this.loadTexture("/app/assets/textures/flake-2.png","flake2Texture"),
-            this.loadTexture("/app/assets/textures/flake-3.png","flake3Texture")
+            this.loadTexture("/app/assets/textures/flake-3.png","flake3Texture"),
+            this.loadTexture("/app/assets/textures/background.jpg","background")
         ]).then(() => {
             this.launchScene()
         })
@@ -73,6 +74,13 @@ export default class App {
 
     launchScene() {
         console.log("Assets chargées")
+        // Adding scene background
+        let background = new THREE.Mesh(
+            new THREE.SphereBufferGeometry(500, 32, 16),
+            new THREE.MeshBasicMaterial({ map: this.texturesArr.background })
+        )
+        background.geometry.scale(-1, 0.5, 1)
+        this.scene.add(background)
 
         // Helpers
         let axesHelper = new THREE.AxesHelper(10)
@@ -83,7 +91,7 @@ export default class App {
         this.scene.add(ambientLight)
 
         let pointLight = new THREE.PointLight(0x00ffff, 1, 20)
-        pointLight.position.set(0, 10, 0)
+        pointLight.position.set(10, 10, 0)
         this.scene.add(pointLight)
 
         let pointLightHelper = new THREE.PointLightHelper(pointLight, 1)
@@ -93,11 +101,46 @@ export default class App {
         this.clock = new THREE.Clock()
         this.currentTime = 0
         this.lastTime = 0
+        
+        // Cube camera
+        let cubeCamera = new THREE.CubeCamera(1, 1000, 256)
+        cubeCamera.renderTarget.texture.minFilter =
+            THREE.LinearMipMapLinearFilter        
 
         // MESHES
-        // Model
+        // MODEL
+
+        // Extract Sphere
+        let sphere = new THREE.Group()
+        let sphereElements = this.modelsArr.model.children.slice(Math.max(this.modelsArr.model.children.length - 3, 1))
+
+        sphereElements.forEach(element => {
+            sphere.add(element)
+        })
+
+        sphere.traverse(child  => {
+            if (child instanceof THREE.Mesh) {
+                if(child.name === "Sphere"){
+                    child.material = new THREE.MeshStandardMaterial({
+                        transparent: true,
+                        opacity: 0.4,
+                        metalness: .2 ,
+                        roughness: .2,
+                        emissive: 0xffffff,
+                        emissiveIntensity: 0.4,
+                        envMap: cubeCamera.renderTarget.texture
+                    })
+
+                    cubeCamera.update(this.renderer, this.scene)
+                }
+            }
+        })
+
+        sphere.position.y += 2
+        this.scene.add(sphere)
+
         this.scene.add(this.modelsArr.model)
-        console.log(this.modelsArr.model)
+
 
         // Fire
         // this.fire = new Fire(this.texturesArr.fireTexture)
